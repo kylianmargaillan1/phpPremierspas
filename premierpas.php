ffeac9ef1ef266c2e8e0cadfacb1dd75dba6dd04
MargaillanKylian <kylian.margaillan@etu.univ-amu.fr>
2021-11-25 09:25:40 +0100

    php

---
+++ premierpas.php
@@ -0,0 +1,18 @@
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>PremierPhp</title>
+</head>
+<body>
+<?php
+$var1 = 6;
+$var2 = 1.3;
+$var3 = 'Variable 3';
+echo '<strong> Voici mon  premier programme PHP  </strong><br/>'  . "\n";
+echo 'C\'est   pas    mal  non  ? <br/>';
+echo   "$var1 + $var2";
+echo   $var1 + $var2;
+echo   $var1 + $var3;
+?>
+</body>
+</html>
