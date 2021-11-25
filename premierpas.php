e206a555b1cc5072abb191447078d06826a89aa2
MargaillanKylian <kylian.margaillan@etu.univ-amu.fr>
2021-11-25 10:54:01 +0100

    exercice 4-6

--- premierpas.php
+++ premierpas.php
@@ -21,8 +21,8 @@
     }
 <?php } ?>
 <?php
-    $jour = date('d/m/Y', strtotime('Y-11-25'));
-    echo $jour."\n" ;
+    $jour = date('d/m/Y', strtotime('2021-11-25'));
+echo $jour, "\n";
     $jour = date('F d, Y, h:i A');
     echo $jour;
 ?>
