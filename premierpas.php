b63e5eda75a8c9c226f6f94bb9958f0c2624c2e9
MargaillanKylian <kylian.margaillan@etu.univ-amu.fr>
2021-11-25 09:51:28 +0100

    exercice 2

--- premierpas.php
+++ premierpas.php
@@ -1,18 +1,23 @@
-<html lang="fr">
-<head>
-    <meta charset="utf-8">
-    <title>PremierPhp</title>
-</head>
-<body>
 <?php
-$var1 = 6;
-$var2 = 1.3;
-$var3 = 'Variable 3';
-echo '<strong> Voici mon  premier programme PHP  </strong><br/>'  . "\n";
-echo 'C\'est   pas    mal  non  ? <br/>';
-echo   "$var1 + $var2";
-echo   $var1 + $var2;
-echo   $var1 + $var3;
+        $title = "Test";
+    function start_page($title)
+    {
+?><!DOCTYPE html>
+<html lang  ="fr">
+<head>
+    <title><?php echo  $title; ?></title>
+</head><body>
+<?php
+    }
 ?>
+<hr/><br/><strong>Test</strong><br/><hr/>
+<?php
+    function end_page($title) {
+        ?>
 </body>
-</html>
+<footer>
+    <title> <?php echo  $title; ?> </title>
+</footer>
+    }
+<?php } ?>
+
