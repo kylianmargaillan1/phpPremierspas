5260b0fd59452a98784fcf5c6b132024a18659a2
MargaillanKylian <kylian.margaillan@etu.univ-amu.fr>
2021-12-02 13:36:03 +0100

    exercice 5-0-6

--- calculator.php
+++ calculator.php
@@ -5,23 +5,23 @@ function start_page($title)
 ?><!DOCTYPE html>
 <html lang  ="fr">
 <head>
-    <title><?php echo $title ?></title>
+    <title>calculator <?php echo $title; ?></title>
 </head><body>
 <?php
 }
 ?>
-
+<form action='calculator.php' method='post'>
 <op1>
-
+    <input type="text" name= "op1"/
 </op1>
-echo '.'
 <op2>
-
+<input type="text" name="op2"
 </op2>
 <input checked="checked" type="radio" name="op" value="*"/>*<br/>
 <input type="radio" name="op" value="+"/>+<br/>
 <input type="radio" name="op" value="-"/>-<br/>
 <input type="radio" name="op" value="/"/>
+</form>
 <?php
 function end_page($title) {
 ?>
